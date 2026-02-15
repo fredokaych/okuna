ba726c7a5484e481b067185b520e9fa5f4f707e2
Fred Okech <89655979+fredokaych@users.noreply.github.com>
2026-02-15 18:07:03 +0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,12 @@
-import AOS from "aos";
-import "aos/dist/aos.css";
+
 
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { HashRouter } from "react-router-dom";
 import App from "./App";
 import "./index.css";
+import AOS from "aos";
+import "aos/dist/aos.css";
 
 AOS.init({
   duration: 1000,
